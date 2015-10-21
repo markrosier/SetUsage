56bc7fa884672edb5f87f14da9da8800e1809249
markrosier <mark.rosier@iotaelectronics.co.uk>
2015-10-21 13:15:27 +0100

    first commit

---
+++ include/params.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   params.h
+ * Author: Iota Electronics Limited
+ *
+ * Created on 27 July 2014, 13:12
+ * for I2R Medical Limited
+ */
+
+#ifndef PARAMS_H
+#define	PARAMS_H
+
+#define RUNNING_HOURS   938
+
+#endif	/* PARAMS_H */
+
